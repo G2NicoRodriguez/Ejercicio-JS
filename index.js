e3746652b5e86f7d8b9fbec760d4152fc33a081a
Nicolás Rodriguez <96657614+G2NicoRodriguez@users.noreply.github.com>
2022-10-28 02:45:14 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+/*Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+
+- Tu nombre (string)
+
+- Tu edad (number)
+
+- ¿Eres desarrollador? (boolean)
+
+- Tu fecha de nacimiento (Date)
+
+- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url) */
+
+let lista= ["Nico", 21, true];
+let fecha_cadena = new Date(" January 19 2001")
+const libro ={
+    titulo: 10,
+    autor: goku,
+    fecha: 19,
+    url: google.com 
+}
